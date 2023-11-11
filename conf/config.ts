1d8e8a435f330c6819d77b676a68333bafc7b825
Muhsin-42 <muhsinofficial21@gmail.com>
2023-11-11 20:06:37 +0530

    Removed metadata

--- conf/config.ts
+++ conf/config.ts
@@ -1,7 +1,7 @@
 const conf = {
     appwriteUrl: String(process.env.NEXT_PUBLIC_API_ENDPOINT),
     appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
-    homeUrl: String(process.env.NEXT_PUBLIC_HOOME_URL),
+    homeUrl: String(process.env.NEXT_PUBLIC_HOME_URL),
     signInUrl: String(process.env.NEXT_PUBLIC_SIGNIN_URL),
     signUpUrl: String(process.env.NEXT_PUBLIC_SIGNUP_URL)
 }
